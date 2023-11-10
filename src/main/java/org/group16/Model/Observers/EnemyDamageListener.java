41966044b80c1e5ce185ae975f3ec3a37e8b4209
Andrethemandre <nanook2000@gmail.com>
2023-11-10 22:28:42 +0100

    fixat playerhealthlisterner. samt lite på damagelistener

---
+++ src/main/java/org/group16/Model/Observers/EnemyDamageListener.java
@@ -0,0 +1,17 @@
+package org.group16.Model.Observers;
+
+import org.group16.Model.GameObjects.Enemy.Enemy;
+
+public class EnemyDamageListener {
+    // suposed to daamage the enemy but need specific enemy to do so both it´s type
+    // and health. also need to add get/set health methods to the enemy class(es?)
+    public void updatehealth(Enemy enemy) {
+        if (enemy.getHealth() > 0) {
+            enemy.setHealth(enemy.getHealth() - 1);
+        }
+    }
+
+    public Boolean isDead() {
+        return enemy.getHealth() == 0;
+    }
+}
---
+++ src/main/java/org/group16/Model/Observers/PlayerDamageListener.java
@@ -0,0 +1,23 @@
+package org.group16.Model;
+
+void updatehealth(){
+
+    if (player.getHealth() > 0 )
+    
+    {
+        player.setHealth(play er.getHealth() - 1);
+       
+    }
+}
+@Override
+Boolean isDead(){
+    if (player.getHealth() == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }```
+}
+}
