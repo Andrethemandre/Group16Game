bbd9e7d6f7fe63e259546a74a4787edaab00afba
linuskar <linuskungalv@gmail.com>
2023-11-10 16:48:05 +0100

    setup controller

---
+++ src/main/java/org/group16/Controller/PlayerController.java
@@ -0,0 +1,58 @@
+package org.group16.Controller;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class PlayerController implements KeyListener, MouseListener {
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mouseClicked'");
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mousePressed'");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mouseReleased'");
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mouseEntered'");
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mouseExited'");
+    }
+    
+}
--- src/main/java/org/group16/View/Renderer.java
+++ src/main/java/org/group16/View/Renderer.java
@@ -1,6 +1,6 @@
 package org.group16.View;
 import java.awt.Graphics;
 
-interface Renderer {
+public interface Renderer {
     public void render(Graphics g);
 }
