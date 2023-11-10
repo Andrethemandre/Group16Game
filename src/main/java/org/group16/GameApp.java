70c6e3e6f47ca59eaf95d86c9f60f738a9a5376b
linuskar <linuskungalv@gmail.com>
2023-11-10 16:35:34 +0100

    setup view

--- src/main/java/org/group16/GameApp.java
+++ src/main/java/org/group16/GameApp.java
@@ -1,8 +1,10 @@
 package org.group16;
 
+import org.group16.View.GameWindow;
+
 public class GameApp {
     public static void main(String[] args) {
-
+        GameWindow mainWindow = new GameWindow();
         System.out.println("Hello world!");
     }
 
