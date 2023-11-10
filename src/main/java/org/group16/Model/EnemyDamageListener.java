8351ee8056dd0d6ec0e17c76191ed758000c3b8c
YahyaMohamed95 <gusyahyamo@student.gu.se>
2023-11-10 18:38:46 +0100

    update

--- src/main/java/org/group16/Model/EnemyDamageListener.java
+++ src/main/java/org/group16/Model/EnemyDamageListener.java
@@ -1,24 +1,22 @@
 package org.group16.Model;
 
-void updatehealth(){
-    if (enemy.getHealth() > 0) 
-    
-    {
-        enemy.setHealth(enemy .getHealth() - 1);
-       
-    }
-}
-Boolean isDead(){
-    if (enemy.getHealth() == 0)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }```
+public class EnemyDamageListener {
 
-
-}
+//    void updatehealth() {
+//        if (enemy.getHealth() > 0) {
+//            enemy.setHealth(enemy.getHealth() - 1);
+//
+//        }
+//    }
+//
+//    boolean isDead() {
+//        if (enemy.getHealth() == 0) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//
+//
+//    }
 
 }
