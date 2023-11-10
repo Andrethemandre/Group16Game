d82b0480beb2a0cac2a01647f57c04c2a859ecf7
Figer-op <90894397+Figer-op@users.noreply.github.com>
2023-11-10 16:44:33 +0100

    created Positionable

--- src/main/java/org/group16/Model/GameObjects/Positionable.java
+++ src/main/java/org/group16/Model/GameObjects/Positionable.java
@@ -1,5 +1,24 @@
 package org.group16.Model.GameObjects;
 
 public abstract class Positionable implements IPositionable {
-    
+
+    private int x;
+    private int y;
+
+    private void setX(int x){
+        this.x = x;
+    }
+
+    private void setY(int y){
+        this.y=y;
+    }
+
+    public int getX(){
+        return this.x;
+    }
+
+    public int getY(){
+        return this.y;
+    }
+
 }
