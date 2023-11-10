e9a132c8691474e4d9b219e04143daebc144a33c
Figer-op <90894397+Figer-op@users.noreply.github.com>
2023-11-10 16:35:04 +0100

    change name for packages

--- src/main/java/org/group16/Model/GameObjects/IPositionable.java
+++ src/main/java/org/group16/Model/GameObjects/IPositionable.java
@@ -1,4 +1,4 @@
-package src.main.java.org.group16.Model.GameObjects;
+package org.group16.Model.GameObjects;
 
 public interface IPositionable {
     //gets x and y
--- src/main/java/org/group16/Model/GameObjects/Positionable.java
+++ src/main/java/org/group16/Model/GameObjects/Positionable.java
@@ -1,4 +1,4 @@
-package src.main.java.org.group16.Model.GameObjects;
+package org.group16.Model.GameObjects;
 
 public abstract class Positionable implements IPositionable {
     
