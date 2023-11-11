d974d724f32daffe1211d038828a38f861b3cccb
YahyaMohamed95 <gusyahyamo@student.gu.se>
2023-11-11 12:49:36 +0100

    removed duplicates

--- src/main/java/org/group16/Model/Observers/ObservableEvents.java
+++ src/main/java/org/group16/Model/Observers/ObservableEvents.java
@@ -1,9 +1,7 @@
 package org.group16.Model.Observers;
 
 public interface ObservableEvents {
-    private void addObservers(){}
-    private void removeObservers(){}
-    private void notifyObservers(){}
+
 
     void addObserver(GameObserver o);
 
