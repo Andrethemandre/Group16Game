0f0cd2c5b97230d37e9047c6127f54246c0c9119
Caramajau <caramia0505@gmail.com>
2023-11-11 13:23:57 +0100

    Added IEnemy interface

--- src/main/java/org/group16/Model/GameObjects/Enemy/Enemy.java
+++ src/main/java/org/group16/Model/GameObjects/Enemy/Enemy.java
@@ -1,18 +1,23 @@
 package org.group16.Model.GameObjects.Enemy;
 
 import org.group16.Model.GameObjects.CollisionBox;
-import org.group16.Model.GameObjects.GameObject;
 import org.group16.Model.GameObjects.Positionable;
+import org.group16.Model.GameObjects.Blocks.BlockType;
 import org.group16.Model.GameObjects.Player.Player;
 import org.group16.Model.Observers.Health;
 
-public abstract class Enemy extends Positionable implements GameObject, Health /* implements Health */ {
+public abstract class Enemy extends Positionable implements IEnemy, Health /* implements Health */ {
     private CollisionBox collisionBox;
     private final EnemyType enemyType;
     private int damage;
 
     Enemy(EnemyType enemyType) {
         this.enemyType = enemyType;
+    }
+
+    @Override
+    public EnemyType getEnemyType() {
+        return enemyType;
     }
 
     public void dealDamage(Player player) {
