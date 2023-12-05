016493659c103690164c2f7cda4b36b2dd0b0414
Caramajau <caramia0505@gmail.com>
2023-12-05 09:26:18 +0100

    Renamed freezeable to freezable

--- src/main/java/org/group16/Model/GameObjects/Enemy/IObstacle.java
+++ src/main/java/org/group16/Model/GameObjects/Enemy/IObstacle.java
@@ -3,10 +3,10 @@ package org.group16.Model.GameObjects.Enemy;
 import org.group16.Model.GameObjects.GameObjectType;
 import org.group16.Model.GameObjects.IGameObject;
 import org.group16.Model.Observers.CanDie;
-import org.group16.Model.Observers.Freezeable;
+import org.group16.Model.Observers.Freezable;
 import org.group16.Model.Observers.HasHealth;
 
-public interface IObstacle extends IGameObject, CanDie, Freezeable {
+public interface IObstacle extends IGameObject, CanDie, Freezable {
     void dealDamage(HasHealth otherGameObject);
     void triggerPowerUp(GameObjectType powerUp);
 }
--- src/main/java/org/group16/Model/Observers/Freezable.java
+++ src/main/java/org/group16/Model/Observers/Freezable.java
@@ -1,6 +1,6 @@
 package org.group16.Model.Observers;
 
-public interface Freezeable {
+public interface Freezable {
     void freeze();
     boolean isFrozen();
 }
