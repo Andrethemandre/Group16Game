4a6daed5367fcf1b0dc3bc3fcdfb41a146c0827c
Figer-op <90894397+Figer-op@users.noreply.github.com>
2023-12-05 10:55:51 +0100

    lebel 378

--- src/main/java/org/group16/Model/Level/LevelFactory.java
+++ src/main/java/org/group16/Model/Level/LevelFactory.java
@@ -10,6 +10,12 @@ public class LevelFactory {
                 return new FirstLevel();
             case 2:
                 return new SecondLevel();
+            case 3:
+                return new Level3();
+            case 7:
+                return new Level7();
+            case 8:
+                return new Level8();
             default:
                 throw new IllegalArgumentException("Level is not supported");
         }
