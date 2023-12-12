46daa27cc02b707fb307e7fc87d8b916c04f1c75
Caramajau <caramia0505@gmail.com>
2023-12-12 12:25:12 +0100

    readded file to match in other branch

---
+++ src/main/java/org/group16/Model/Observers/ObservableEvents.java
@@ -0,0 +1,9 @@
+package org.group16.Model.Observers;
+
+public interface ObservableEvents {
+    void addObserver(GameObserver o);
+
+    void removeObserver(GameObserver o);
+
+    void notifyObservers();
+}
